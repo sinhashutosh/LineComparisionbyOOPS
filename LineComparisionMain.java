e72f6c0eb69ae3ef9243e335f75bed166670b363
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-22 20:39:53 +0530

    Code updated accordinf to UC2

--- LineComparisionMain.java
+++ LineComparisionMain.java
@@ -1,8 +1,19 @@
 package org.example;
 
-public class LineComparisionMain{
+public class LineComparisionMain {
     public static void main(String[] args) {
-        LengthCalculator length = new LengthCalculator();
-        length.calculate();
+        double length_0f_line1;
+        double length_0f_line2;
+        LengthCalculator length1 = new LengthCalculator();
+        System.out.println("Enter the Co-ordinate of first line ");
+        length_0f_line1 = length1.calculate();
+        System.out.println("**************************************************");
+        System.out.println("Enter the Co-ordinate of second line");
+        LengthCalculator length2 = new LengthCalculator();
+        length_0f_line2 = length2.calculate();
+        System.out.println("**************************************************");
+        System.out.println("Method invoked to compare line");
+        Compare isEqual = new Compare();
+        isEqual.comparision(length_0f_line1, length_0f_line2);
     }
 }
