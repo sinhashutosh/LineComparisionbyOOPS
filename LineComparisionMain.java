172c7581459a881203cf4b6d25d379ea1b0e058f
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-22 20:03:25 +0530

    This is main class

---
+++ LineComparisionMain.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class LineComparisionMain{
+    public static void main(String[] args) {
+        LengthCalculator length = new LengthCalculator();
+        length.calculate();
+    }
+}
